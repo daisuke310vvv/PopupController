8a53c54bb5100e15303c20010df0d1c7dbc74a12
Daisuke Sato <daisuke.sato1991@gmail.com>
2016-02-04 19:31:59 +0900

    Initial Commit

---
+++ PopupController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopupController
+//
+//  Created by 佐藤 大輔 on 2/4/16.
+//  Copyright © 2016 Daisuke Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
